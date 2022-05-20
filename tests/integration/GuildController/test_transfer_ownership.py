661304b420bd3b458a29339e682444e5a1e6d899
colin-vrh <colin@versaillesheroes.com>
2022-05-20 12:11:47 +0800

    + added transfer_ownership test.

---
+++ tests/integration/GuildController/test_transfer_ownership.py
@@ -0,0 +1,45 @@
+import brownie
+
+def test_transfer_guild_controller_ownership(accounts, guild_controller):
+    alice, bob, carl = accounts[:3]
+
+    assert(guild_controller.admin() == alice)
+
+    with brownie.reverts('dev: admin only'):
+        guild_controller.commit_transfer_ownership(bob, {"from": carl})
+
+    # commit ownership
+    guild_controller.commit_transfer_ownership(bob, {"from": alice})
+    
+    assert(guild_controller.future_admin() == bob)
+
+    with brownie.reverts('dev: admin only'):
+        guild_controller.apply_transfer_ownership({"from": carl})
+
+    # apply commit
+    guild_controller.apply_transfer_ownership({"from": alice})
+
+    assert(guild_controller.admin() == bob)
+
+
+def test_transfer_create_guild_ownership(accounts, guild_controller):
+    alice, bob, carl = accounts[:3]
+
+    assert(guild_controller.create_guild_admin() == alice)
+
+    with brownie.reverts('dev: admin only'):
+        guild_controller.commit_transfer_ownership(bob, {"from": carl})
+
+    # commit ownership
+    guild_controller.commit_transfer_create_guild_ownership(bob, {"from": alice})
+    
+    assert(guild_controller.future_create_guild_admin() == bob)
+
+    with brownie.reverts('dev: admin only'):
+        guild_controller.apply_transfer_create_guild_ownership({"from": carl})
+
+    # apply commit
+    guild_controller.apply_transfer_create_guild_ownership({"from": alice})
+
+    assert(guild_controller.create_guild_admin() == bob)
+
